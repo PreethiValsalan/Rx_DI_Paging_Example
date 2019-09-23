1af706f17f653d7da3272312456fea4a1a8c2270
preethivalsalan <preethivalsalan2@gmail.com>
2019-09-23 20:16:17 +0300

    test automation

---
+++ app/src/androidTest/java/com/interapp/developer/rxpagingex/UITest.java
@@ -0,0 +1,95 @@
+package com.interapp.developer.rxpagingex;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.KeyEvent;
+import android.widget.EditText;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.ViewFinder;
+import androidx.test.filters.LargeTest;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+import androidx.test.uiautomator.By;
+import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.Until;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.pressKey;
+import static androidx.test.espresso.action.ViewActions.pressMenuKey;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class UITest {
+    private String stringToBetyped;
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule
+            = new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void initValidString() {
+        // Specify a valid string.
+        stringToBetyped = "Espresso";
+    }
+
+    private static final String BASIC_SAMPLE_PACKAGE
+            = "com.interapp.developer.rxpagingex";
+    private static final int LAUNCH_TIMEOUT = 5000;
+    private static final String STRING_TO_BE_TYPED = "UiAutomator";
+    private UiDevice device;
+
+    @Before
+    public void startMainActivityFromHomeScreen() {
+        // Initialize UiDevice instance
+        device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+
+        // Start from the home screen
+        device.pressHome();
+
+        // Wait for launcher
+        final String launcherPackage = device.getLauncherPackageName();
+        assertThat(launcherPackage, notNullValue());
+        device.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)),
+                LAUNCH_TIMEOUT);
+
+        // Launch the app
+        Context context = ApplicationProvider.getApplicationContext();
+        final Intent intent = context.getPackageManager()
+                .getLaunchIntentForPackage(BASIC_SAMPLE_PACKAGE);
+        // Clear out any previous instances
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        context.startActivity(intent);
+
+        // Wait for the app to appear
+        device.wait(Until.hasObject(By.pkg(BASIC_SAMPLE_PACKAGE).depth(0)),
+                LAUNCH_TIMEOUT);
+    }
+
+    @Test
+    public void checkViewExists() {
+
+      onView(withId(R.id.mainGridView)).perform();
+    }
+
+    @Test
+    public void testSearchBar() {
+
+        onView(withId(R.id.search)).perform(click());
+        onView(isAssignableFrom(EditText.class)).perform(typeText("nature"), pressKey(KeyEvent.KEYCODE_ENTER));
+    }
+}
